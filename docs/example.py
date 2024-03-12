313fdf49f0c7d598664193091f79e603787dbac2
purplelemons-dev <ironbreaker2005@gmail.com>
2024-03-12 12:43:43 -0500

    Add directory structure documentation to example.py

--- docs/example.py
+++ docs/example.py
@@ -1,12 +1,31 @@
 # Example GRAPI server setup
 
+"""
+Sample directory structure:
+```
+.
+|-- server.py
+|-- views
+|   |-- .html
+|   |-- .css
+|   |-- .js
+|   |-- about
+|   |   |-- .html
+|   |   |-- .css
+|   |   |-- .js
+|   |-- favicon.ico
+```
+Views are automatically served.
+"""
+
 import grapi
 
 server = grapi.Server()
 
 
 @server("/hello")
-def GET(request, response):
+def GET(request: grapi.Request, response: grapi.Response) -> grapi.Response:
     return response.text("Hello, world!")
+
 
 server.run()
