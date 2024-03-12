a61fcc36274a9a5a381d2ecac5c04f0890071889
purplelemons-dev <ironbreaker2005@gmail.com>
2024-03-12 18:02:34 -0500

    Refactor Headers class to use a dictionary for storing headers

--- src/grapi/headers.py
+++ src/grapi/headers.py
@@ -4,46 +4,29 @@ from typing import Iterator, Callable
 __all__ = ["Headers"]
 
 
-def ensure_format_name(func: Callable) -> Callable:
-    def wrapper(self: Headers, name: str, *args, **kwargs):
-        return func(self, format_name(name), *args, **kwargs)
-
-    return wrapper
-
-
 def format_name(name: str) -> str:
     return name.replace("_", "-").lower()
 
 
 class Headers:
 
-    @staticmethod
-    def __init__(self, **kwargs: dict[str,str]):
-        self._headers = {}
+    def __init__(self, **kwargs: dict[str, str]):
+        self.headers: dict[str, str] = {}
 
         for name, value in kwargs.items():
-            self._headers[format_name(name)] = value
+            self.headers[format_name(name)] = value
+
+    def __str__(self) -> str:
+        return "\r\n".join(f"{name}: {value}" for name, value in self.headers.items())
 
     def __getitem__(self, key: str) -> str:
-        return self._headers[key]
+        return self.headers[format_name(key)]
 
     def __setitem__(self, key: str, value: str) -> None:
-        self._headers[key] = value
+        self.headers[format_name(key)] = value
 
     def __delitem__(self, key: str) -> None:
-        del self._headers[key]
+        del self.headers[format_name(key)]
 
     def __iter__(self) -> Iterator[str]:
-        return iter(self._headers)
-
-    @ensure_format_name
-    def __getattr__(self, name: str) -> str:
-        return self._headers[name]
-
-    @ensure_format_name
-    def __setattr__(self, name: str, value: str) -> None:
-        self._headers[name] = value
-
-    @ensure_format_name
-    def __delattr__(self, name: str) -> None:
-        del self._headers[name]
+        return iter(self.headers)
